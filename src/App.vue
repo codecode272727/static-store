5fd4bbb2412cb92c040c56a4dd4b97f920474072
codecode272727 <codecode272727@gmail.com>
2021-08-01 15:08:31 +0800

    'axios'

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>Hello {{ value }}
-      <p>Test api connection</p>
-  </div>
+    <div>
+
+        <div>Hello {{ value }}</div>
+        <p>Test api connectiong</p>
+        <div v-for='company in companies' :key='company.id'>
+            <h3>Company Name: </h3>  {{company.name}}
+            <h3>Company Address: </h3>  {{company.address}}
+        </div>
+
+    </div>
   
 </template>
 
@@ -11,7 +18,17 @@ export default {
   data() {
     return {
       value: "World"
-    };
+    }
+  },
+
+  computed: {
+    companies() {
+        return this.$store.state.companies
+    }
+  },
+
+  mounted() {
+      this.$store.dispatch("getCompanies");
   }
 };
 </script>
---
+++ src/store/index.js
@@ -0,0 +1,38 @@
+﻿import axios from 'axios'
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex);
+
+const state = {
+    companies: []
+}
+
+
+const getters = {}
+
+
+const actions = {
+    getCompanies({ commit }) {
+        axios.get('https://webapi20210720.azurewebsites.net/api/companies')
+            .then(response => {
+                commit('SET_COMPANIES', response.data)
+            })
+    }
+
+}
+
+
+const mutations = {
+    SET_COMPANIES(state, companies) {
+        state.companies = companies
+    }
+}
+
+
+export default new Vuex.Store({
+    state,
+    getters,
+    actions,
+    mutations
+})
